47b60c35ff7b48685d59a33f7a093a2916cdba02
edelweissidestory-rgb <edelweissidestory@gmail.com>
2026-02-22 04:33:42 +0700

    Update absensi_dashboard.py

--- absensi_dashboard.py
+++ absensi_dashboard.py
@@ -156,14 +156,14 @@ if mode == "Admin" and password == "risum771":
     # ================= BULANAN =================
     with tab2:
 
-        bulan = st.selectbox("Bulan", list(range(1,13)))
-        tahun = st.selectbox("Tahun", list(range(2024,2031)))
+    bulan = st.selectbox("Bulan", list(range(1,13)))
+    tahun = st.selectbox("Tahun", list(range(2024,2031)))
 
-       res = supabase.table("absensi")\
-            .select('id,tanggal,jam_masuk,jam_pulang,status,keterangan,nama:nama_id(nama),posisi:posisi_id(posisi)')\
-            .gte("tanggal", f"{tahun}-{str(bulan).zfill(2)}-01")\
-            .lte("tanggal", f"{tahun}-{str(bulan).zfill(2)}-31")\
-            .execute()
+    res = supabase.table("absensi")\
+        .select('id,tanggal,jam_masuk,jam_pulang,status,keterangan,nama:nama_id(nama),posisi:posisi_id(posisi)')\
+        .gte("tanggal", f"{tahun}-{str(bulan).zfill(2)}-01")\
+        .lte("tanggal", f"{tahun}-{str(bulan).zfill(2)}-31")\
+        .execute()
 
         if res.data:
             rows = []
@@ -198,6 +198,7 @@ if mode == "Admin" and password == "risum771":
 
         else:
             st.info("Belum ada data bulan ini")
+
 
 
 
